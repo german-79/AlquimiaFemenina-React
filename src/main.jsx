f3ff7824bdd0e5b1fd8307d5caf77cb89cff347a
German <german.oliverapere@gmail.com>
2025-07-22 15:46:55 -0300

    corrijo rutoas para pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import InstitucionalPage from "./pages/InstitucionalPage.jsx";
@@ -11,23 +11,19 @@ import FormularioPage from "./pages/FormularioPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <HomePage /> },
-      { path: "/institucional", element: <InstitucionalPage /> },
-      { path: "/servicios", element: <ServiciosPage /> },
-      { path: "/tienda", element: <TiendaPage /> },
-      { path: "/formulario", element: <FormularioPage /> },
-      { path: "*", element: <NotFoundPage /> },
-    ],
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="institucional" element={<InstitucionalPage />} />
+          <Route path="servicios" element={<ServiciosPage />} />
+          <Route path="tienda" element={<TiendaPage />} />
+          <Route path="formulario" element={<FormularioPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
